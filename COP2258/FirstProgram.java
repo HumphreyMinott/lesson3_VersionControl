4439f0f9cf7fdb4f617f950684fc1e7d9c4f1352
HumphreyMinott <hcm13c@my.fsu.edu>
2017-02-03 23:37:57 +0000

    First commit for lesson 3

---
+++ COP2258/FirstProgram.java
@@ -0,0 +1,9 @@
+public class FirstProgram {
+    
+    public static void main (String[] args) {
+      
+        
+        
+    }//End of Main
+    
+}//End of FirstProgram class
---
+++ COP2258/Lesson_3/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+        //Display Message Hello World on the console
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }//End of Main
+    
+}//End of HelloWorld class
